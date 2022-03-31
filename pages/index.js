db95a453b613a3f09d6dc2037769c4ad4a106252
jasonrudin <j.b.rudin@gmail.com>
2022-03-31 10:43:07 -0400

    transitioned from typescript to javascript

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home(){
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+
+      </header>
+      <div>
+
+      </div>
+      <footer>
+        
+      </footer>
+
+    </div>
+  )
+}
